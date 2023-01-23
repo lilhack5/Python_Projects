4da556b1e870dbe2ce9dc52811ba6ce9c5bf580f
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-01-23 12:41:29 -0800

    loading all so far
    This is what I have learned so far in python

---
+++ learning/notes.py
@@ -0,0 +1,14 @@
+# This is my note.
+
+"""This is my note
+and this is still my note
+and this again is still my note.
+"""
+
+def printMe():
+    """this is my docstring
+    """
+
+    print("Meeeeeeeeeeeeeeee!")
+
+print(printMe.__doc__)
