9facab2b676dcc4b048df8c142e08ecd17f290f2
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-01-23 12:59:14 -0800

    updated code
    made some updates to nice or mean

--- Nice_or_Mean_tutorial.py
+++ Nice_or_Mean_tutorial.py
@@ -75,9 +75,8 @@ def score(nice,mean,name):
 
 
 def win(nice,mean,name):
-    dub = "you win"
     # Substitute the {} wildcards with our variable values
-    print("\nNice job {},", + dub.upper(), + "! \nEveryone loves you and you've \nmade lots of friends along the way!".format(name))
+    print("\nNice job {}, YOU WIN! \nEveryone loves you and you've \nmade lots of friends along the way!".format(name))
     # call again function and pass in our variables
     again(nice,mean,name)
 
