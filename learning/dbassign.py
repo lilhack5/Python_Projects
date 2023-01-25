2fd58bb86fb3e5d6046e4e5c3f2611a138c64e27
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-01-25 13:04:09 -0800

    added list
    made many changes to dbassignment

--- learning/dbassign.py
+++ learning/dbassign.py
@@ -11,6 +11,8 @@ import sqlite3
 # Creates a new database.
 conn = sqlite3.connect('test2.db')
 
+fileList = ('information.docx', 'Hello.txt', 'myImage.png',\
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
 
 with conn:
     cur = conn.cursor()
@@ -19,38 +21,12 @@ with conn:
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_file TEXT\
         )")
+    for file in fileList:
+            if file.endswith(".txt"):
+                msg = "File: {}".format(file[0])
+                print(file)
+    
     conn.commit()
 # close connection to database
 conn.close()
 
-conn = sqlite3.connect('text2.db')
-
-# add data into tbl_test2
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('information.docx'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('Hello.txt'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('myImage.png'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('myMovies.mpg'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('World.txt'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('data.pdf'))
-    cur.execute("INSERT INTO tbl_test2(col_file) VALUES (?)", \
-                ('myPhoto.jpg'))
-    conn.commit()
-conn.close()
-
-conn = sqlite3.connect('test2.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_file WHERE col_file = '.txt'")
-    varType = cur.fetchall()
-    for item in varType:
-        msg = "File: {}".format(item[0])
-    print(msg)
