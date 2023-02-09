10d85961638ef82dc4041775da4b35f8fc06b757
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-02-09 10:43:51 -0800

    fixed code
    had period in wrong spot

--- learning/polymorphism.py
+++ learning/polymorphism.py
@@ -17,7 +17,7 @@ class Player:
         entry_email = input("Enter your email: ")
         entry_password = input("Enter your Password: ")
         if (entry_email == self.email and entry_password == self.password):
-            print("You have returned again, {}!."format(entry_name))
+            print("You have returned again, {}!.".format(entry_name))
         else:
             print("The password or email is incorrect.")
 
@@ -26,12 +26,25 @@ class Game(Player):
     game_tokens = 555
     game_completed = "55%"
 
+# Child class 2
 class Apex(Player):
     apex_rank = "Gold"
     apex_kd = "1.5"
+    apex_pin = "2355"
 
-customer = Player():
-customer.getLoginInfor()
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_pin = input("Enter your pin: ")
+        if (entry_name == self.email and entry_pin == self.pin_number):
+            print("Welcome, {}!".format(entry_name))
+        else:
+            print("The pin you or email you used is wrong.")
+
+    
+
+customer = Player()
+customer.getLoginInfo()
 
 apex = Game()
 apex.getLoginInfo()
